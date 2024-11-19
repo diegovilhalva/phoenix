85b7562d62ade2547702275fe2fbeeff229c0f5d
Diego Vilhalva <diegorocha450@yahoo.com>
2024-11-19 08:33:48 -0300

    project setup

---
+++ src/routers/routes.jsx
@@ -0,0 +1,17 @@
+import  {createBrowserRouter} from "react-router-dom"
+
+import App from "../App"
+import Register from "../pages/Register"
+
+const router  = createBrowserRouter([
+    {
+        path:'/',
+        element:<App />
+    },
+    {
+        path:'/register',
+        element:<Register />
+    },
+])
+
+export default router
